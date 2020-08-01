6130cafe73124c692972d7c54aa9f0a17f184f1f
IvaK <iva.kozarova@gmail.com>
2020-08-01 06:46:22 +0300

    Add exam problems

---
+++ ValidUsername/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ValidUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ").ToArray();
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                bool isValid = true;
+                if (usernames[i].Length >= 3 && usernames[i].Length <= 16)
+                {
+                    foreach (var letter in usernames[i])
+                    {
+                        if (!(char.IsLetterOrDigit(letter) || letter == '-' || letter == '_'))
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    isValid = false;
+                }
+                if(isValid == true)
+                {
+                    Console.WriteLine(usernames[i]);
+                }
+            }
+        }
+    }
+}
---
+++ WarriorQuest_Dec2019/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace WarriorQuest_Dec2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string cmd;
+
+            while ((cmd = Console.ReadLine()) != "For Azeroth")
+            {
+                string[] cmdArg = cmd.Split();
+                string command = cmdArg[0];
+                if (command == "GladiatorStance" && cmdArg.Length == 1)
+                {
+                    input = input.ToUpper();
+                    Console.WriteLine(input);
+                }
+                else if (command == "DefensiveStance" && cmdArg.Length == 1)
+                {
+                    input = input.ToLower();
+                    Console.WriteLine(input);
+                }
+                else if (command == "Dispel" && cmdArg.Length == 3)
+                {
+                    int index = int.Parse(cmdArg[1]);
+                    char letter = char.Parse(cmdArg[2]);
+                    if (index >= 0 && index < input.Length)
+                    {
+                        var inputArr = input.ToArray();
+                        inputArr[index] = letter;
+                        input = new string(inputArr);
+                        Console.WriteLine("Success!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dispel too weak.");
+                    }
+                }
+                else if (cmdArg[1] == "Change" && cmdArg.Length == 4)
+                {
+                    string firstSubstring = cmdArg[2];
+                    string secondSubstring = cmdArg[3];
+                    input = input.Replace(firstSubstring, secondSubstring);
+
+                    Console.WriteLine(input);
+                }
+                else if (cmdArg[1] == "Remove" && cmdArg.Length == 3)
+                {
+                    string stringToRemove = cmdArg[2];
+                    while (input.Contains(stringToRemove))
+                    {
+                        int startIndex = input.IndexOf(stringToRemove);
+                        input = input.Remove(startIndex, stringToRemove.Length);
+                    }
+                    Console.WriteLine(input);
+                }
+
+                else
+                {
+                    Console.WriteLine("Command doesn't exist!");
+                }
+            }
+
+        }
+
+    }
+}
+
