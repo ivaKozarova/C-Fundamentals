1170d2db00d095281ac56b73ae099f99407de03f
IvaK <iva.kozarova@gmail.com>
2020-08-02 07:19:31 +0300

    add new

--- Deciphering_April2019/Program.cs
+++ Deciphering_April2019/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Deciphering_April2019
 {
@@ -6,7 +9,28 @@ namespace Deciphering_April2019
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string code = Console.ReadLine();
+            string pattern = @"^([d-z#,\|]+)$";
+            string[] input = Console.ReadLine().Split();
+
+            Match match = Regex.Match(code, pattern);
+            if (match.Success)
+            {
+                string text = match.Groups[1].Value;
+                StringBuilder sb = new StringBuilder();
+                foreach (var letter in text)
+                {
+                    sb.Append((char)(letter - 3));
+                }
+
+                sb.Replace(input[0], input[1]);
+                Console.WriteLine(sb);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+
         }
     }
 }
