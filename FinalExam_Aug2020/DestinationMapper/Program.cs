f685c9f32bbbaf12fb8b9f73982a6b7c940aacc1
IvaK <iva.kozarova@gmail.com>
2020-08-09 14:05:43 +0300

    FinalExam_August2020

---
+++ FinalExam_Aug2020/DestinationMapper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DestinationMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(=|\/)([A-Z][A-Za-z]{2,})\1";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            int travelPoints = 0;
+            var destinations = new List<string>();
+
+            if(matches.Any())
+            {
+                travelPoints = AddMatchToList(matches, travelPoints, destinations);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+
+        private static int AddMatchToList(MatchCollection matches, int travelPoints, List<string> destinations)
+        {
+            foreach (var match in matches)
+            {
+                string destination = match.ToString();
+                destination = destination.Substring(1, destination.Length - 2);
+                travelPoints += destination.Length;
+                destinations.Add(destination);
+            }
+
+            return travelPoints;
+        }
+    }
+}
---
+++ FinalExam_Aug2020/World Tour/StartUp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace WorldTour
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string command;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                var cmdArg = command.Split(":");
+                string toDo = cmdArg[0];
+
+                if (toDo == "Add Stop")
+                {
+                    text = AddStop(text, cmdArg);
+                }
+                else if (toDo == "Remove Stop")
+                {
+                    text = RemoveStop(text, cmdArg);
+                }
+                else if (toDo == "Switch")
+                {
+                    text = Switch(text, cmdArg);
+                }
+
+                Console.WriteLine(text);
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {text}");
+
+        }
+
+        private static string Switch(string text, string[] cmdArg)
+        {
+            string oldString = cmdArg[1];
+            string newString = cmdArg[2];
+            if (text.Contains(oldString))
+            {
+                text = text.Replace(oldString, newString);
+            }
+
+            return text;
+        }
+
+        private static string RemoveStop(string text, string[] cmdArg)
+        {
+            int startIndex = int.Parse(cmdArg[1]);
+            int endIndex = int.Parse(cmdArg[2]);
+            if (startIndex >= 0 && endIndex < text.Length && startIndex <= endIndex)
+            {
+                text = text.Remove(startIndex, endIndex - startIndex + 1);
+            }
+
+            return text;
+        }
+
+        private static string AddStop(string text, string[] cmdArg)
+        {
+            int index = int.Parse(cmdArg[1]);
+            string stringToInsert = cmdArg[2];
+            if (index >= 0 && index <= text.Length)
+            {
+                text = text.Insert(index, stringToInsert);
+            }
+
+            return text;
+        }
+    }
+}
