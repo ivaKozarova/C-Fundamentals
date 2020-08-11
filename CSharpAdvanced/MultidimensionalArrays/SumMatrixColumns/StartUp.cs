05c1e62d9a42371fecc8022a054506dbd3925520
IvaK <iva.kozarova@gmail.com>
2020-08-11 12:04:36 +0300

    MultiDims Arrays

---
+++ CSharpAdvanced/MultidimensionalArrays/SumMatrixColumns/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = dimentions[0];
+            int columns = dimentions[1];
+
+            int[,] matrix = new int[rows, columns];
+            FillMatrix(rows, columns, matrix);
+            PrintSumOfCol(matrix);
+
+        }
+
+        private static void PrintSumOfCol(int[,] matrix)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sumOfCol = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sumOfCol += matrix[row, col];
+                }
+                Console.WriteLine(sumOfCol);
+            }
+        }
+
+        private static void FillMatrix(int rows, int columns, int[,] matrix)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < columns; col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/MultidimensionalArrays/SymbolInMatrix/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            FillMatrix(n, matrix);
+
+            char symbol = char.Parse(Console.ReadLine());
+            SearchForSymbol(n, matrix, symbol);
+        }
+
+        private static void SearchForSymbol(int n, int[,] matrix, char symbol)
+        {
+            bool isFound = false;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound == true)
+                {
+                    break;
+                }
+            }
+            if (isFound == false)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix ");
+            }
+        }
+
+        private static void FillMatrix(int n, int[,] matrix)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                char[] elements = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+        }
+    }
+}
